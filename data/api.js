7bbe9b12d6cdd423f7888f37feb4b0c8e1a61dd5
Simamnkele <smambo85@gmail.com>
2024-02-26 18:37:35 +0200

    Removed comment

--- data/api.js
+++ data/api.js
@@ -27,7 +27,6 @@ const axios = require('axios');
 
 const app = express();
 
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=DI7EUUTTCC5JGMGI';
 
 app.get('/stockData', async (req, res) => {
