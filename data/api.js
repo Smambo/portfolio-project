66a0f19599a2981cfd3c3bc92bd3d23c62b299b5
Simamnkele <smambo85@gmail.com>
2024-02-26 18:35:34 +0200

    Refactored stock api code

--- data/api.js
+++ data/api.js
@@ -1,5 +1,5 @@
 // Function to fetch stock prices from Alpha Vantage API for multiple symbols
-function fetchStockPrices(symbols) {
+/*function fetchStockPrices(symbols) {
     const apiKey = 'DI7EUUTTCC5JGMGI';
     const apiUrl = `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${symbols.join(',')}&apikey=${apiKey}`;
 
@@ -19,7 +19,27 @@ function fetchStockPrices(symbols) {
 }
 
 // Call the function with an array of sample stock symbols
-fetchStockPrices(['AAPL', 'GOOGL', 'MSFT']);
+fetchStockPrices(['AAPL', 'GOOGL', 'MSFT']); */
 
+'use strict';
+const express = require('express');
+const axios = require('axios');
 
+const app = express();
 
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=DI7EUUTTCC5JGMGI';
+
+app.get('/stockData', async (req, res) => {
+    try {
+      const response = await axios.get(url);
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch stock data' });
+    }
+});
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
